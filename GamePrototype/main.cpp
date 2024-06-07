39765ffbf24f565d00af4a5157439431f80587ca
Mathies Van den Bulcke <mathies.van.den.bulcke@student.howest.be>
2024-06-07 10:50:07 +0200

    Change name

--- GamePrototype/main.cpp
+++ GamePrototype/main.cpp
@@ -12,7 +12,7 @@ int SDL_main(int argv, char** args)
 
 	StartHeapControl();
 
-	Game* pGame{ new Game{ Window{ "Vampire game Prototype - Van den Bulcke, Mathies - 1DAE16", 900.f , 500.f } } };
+	Game* pGame{ new Game{ Window{ "Monster Munch - Van den Bulcke, Mathies - 1DAE16", 900.f , 500.f } } };
 	pGame->Run();
 	delete pGame;
 
